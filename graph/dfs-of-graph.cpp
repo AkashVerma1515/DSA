a8be12bd3f235d04ac41c577b82eb861a6b0040b
Akash Verma <akashzxc786@gmail.com>
2021-06-04 01:21:25 +0530

    intial

--- graph/dfs-of-graph.cpp
+++ graph/dfs-of-graph.cpp
@@ -5,30 +5,34 @@ using namespace std;
  // } Driver Code Ends
 
 
-
-class Solution
+class Solution 
 {
     public:
-    //Function to return Breadth First Traversal of given graph.
-	vector<int>bfsOfGraph(int V, vector<int> adj[])
+    
+    vector<int> ans;
+    
+    void dfs(int i, vector<int> &vis, vector<int> adj[])
+    {
+        ans.push_back(i);
+        vis[i] = 1;
+        for(auto it : adj[i])
+        {
+            if(!vis[it])
+            {
+                vis[it] = 1;
+                dfs(it,vis,adj);
+            }
+        }
+    }
+    
+	vector<int>dfsOfGraph(int V, vector<int> adj[])
 	{
-	    vector<int> ans;
 	    vector<int> vis(V,0);
-	    queue<int> q;
-	    q.push(0);
-	    vis[0] = 1;
-	    while(!q.empty())
+	    for(int i=0;i<V;i++)
 	    {
-	        int node = q.front();
-	        q.pop();
-	        ans.push_back(node);
-	        for(auto it : adj[node])
+	        if(!vis[i])
 	        {
-	            if(!vis[it])
-	            {
-	                q.push(it);
-	                vis[it] = 1;
-	            }
+	            dfs(i,vis,adj);
 	        }
 	    }
 	    return ans;
@@ -50,12 +54,12 @@ int main(){
     		int u, v;
     		cin >> u >> v;
     		adj[u].push_back(v);
-    // 		adj[v].push_back(u);
+    		adj[v].push_back(u);
     	}
         // string s1;
         // cin>>s1;
         Solution obj;
-        vector<int>ans=obj.bfsOfGraph(V, adj);
+        vector<int>ans=obj.dfsOfGraph(V, adj);
         for(int i=0;i<ans.size();i++){
         	cout<<ans[i]<<" ";
         }
