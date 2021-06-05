24b157796fe762bfaf23dcebe4a223421c569d00
Akash Verma <akashzxc786@gmail.com>
2021-06-05 16:01:00 +0530

    initial

---
+++ leetcode/arrays/sort-colors.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int z=0,o=0,t=0;
+        int n = nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==0)
+            {
+                z++;
+            }
+            else if(nums[i]==1)
+            {
+                o++;
+            }
+            else if(nums[i]==2)
+            {
+                t++;
+            }
+        }
+        for(int i=0;i<z;i++)
+        {
+            nums[i] = 0;
+        }
+        for(int i=z;i<o+z;i++)
+        {
+            nums[i] = 1;
+        }
+        for(int i=o+z;i<n;i++)
+        {
+            nums[i] = 2;
+        }
+    }
+};
